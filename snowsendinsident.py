212fb6fb3d37ac21ba7a8a174e3281d416baf69f
EashwarRaghunathan <eashwar.splunk@gmail.com>
2014-12-06 21:39:52 +0530

    Create snowsendinsident.py

---
+++ snowsendinsident.py
@@ -0,0 +1,35 @@
+# ./snowsendinsident.py "$sysparm_action" "$category" "$impact" "$urgency" "$caller_id" "$cmdb_ci" "$short_description"  << usage
+import sys
+import urllib2
+import json
+from requests.auth import HTTPBasicAuth
+import requests
+# This py file is a stand-alone integration, it directly works with Splunk
+payload = {
+   "sysparm_action":sys.argv[1],
+   "category":sys.argv[2],
+   "impact":sys.argv[3],
+   "urgency":sys.argv[4],
+   "caller_id":sys.argv[5],
+   "cmdb_ci":sys.argv[6],
+   "short_description":sys.argv[7]
+   "work_notes":sys.argv[7]
+}
+
+
+auth = HTTPBasicAuth("admin", "admin")
+uri = "https://demo002.service-now.com/incident.do?JSONv2"
+
+# http headers for request
+headers = {
+    "Accept": "application/json;charset=utf-8",
+    "Content-Type": "application/json"
+}
+
+#we insert a insident
+r = requests.post(url=uri, data=json.dumps(payload), auth=auth, verify=False, headers=headers)
+
+content = r.json()
+assert (r.status_code == 200)
+print "Response Status Code: " + str(r.status_code)
+print "Response JSON Content: " + str(content)
